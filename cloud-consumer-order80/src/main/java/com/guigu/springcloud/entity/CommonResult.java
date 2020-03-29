1ffd234a093dd937304c19c001f1555daeeb3b85
leishaolin <http://gitlab.cttyun.cn/greatedu/market/backend.git>
2020-03-29 22:00:50 +0800

    新建订单模块

---
+++ cloud-consumer-order80/src/main/java/com/guigu/springcloud/entity/CommonResult.java
@@ -0,0 +1,26 @@
+package com.guigu.springcloud.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+
+    public CommonResult(Integer code,String message){
+
+        this(code,message,null);
+    }
+}
--- cloud-provicer-payment8001/src/main/java/com/guigu/springcloud/controller/PaymentController.java
+++ cloud-provicer-payment8001/src/main/java/com/guigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.guigu.springcloud.entity.CommonResult;
 import com.guigu.springcloud.entity.Payment;
 import com.guigu.springcloud.service.PaymentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +16,15 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
-    @PostMapping("/add")
+    @PostMapping("/payment/add")
     public CommonResult create(Payment payment){
 
         int result = paymentService.create(payment);
         return new CommonResult(200,"ok",result);
     }
 
-    @GetMapping("/getPaymentById")
-    public CommonResult getPaymentById(Long id){
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
 
         Payment paymentById = paymentService.getPaymentById(id);
         return new CommonResult(200,"ok",paymentById);
